ee496276c79c32e70925500c696610744d0bee20
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-08-29 13:18:36 +0300

    apd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Locale;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+
+		HW1();
+		HW2();
+		HW3();
+	}
+
+	static void HW1() {
+
+		String firstName = "Ivan";
+		String middleName = "Ivanovich";
+		String lastName = "Ivanov";
+		String fullName = lastName + " " + middleName + " " + firstName;
+
+		System.out.println("ФИО сотрудника — " + fullName);
+	}
+
+	static void HW2() {
+
+		String firstName = "Ivan";
+		String middleName = "Ivanovich";
+		String lastName = "Ivanov";
+		String fullName = lastName + " " + middleName + " " + firstName;
+
+		System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+
+	}
+
+	static void HW3() {
+
+		String fullName = "Иванов Семён Семёнович";
+		System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.replace("ё", "е"));
+
 	}
 }
